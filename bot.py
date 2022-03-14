2f9cbaa806b9bf43d8a1e33d4df30a2ecff05e64
Toneh <88310222+TonehPy@users.noreply.github.com>
2022-03-14 17:44:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,6 +39,6 @@ for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     bot.load_extension(f'cogs.{filename[:-3]}')
   
-bot.run('token here')
+bot.run('OTQ3MDY0MDY4MTE4ODA2NTM4.Yhn0JQ.eA_rUswliJ4UqivEaOv_SfPUMII')
 
 # A simple Discord bot base made in Python by Toneh#3391 on Discord
