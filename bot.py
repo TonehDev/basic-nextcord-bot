e7dd74940c04401bff57a8bb6be1000fd0e5f7d1
Toneh <88310222+TonehPy@users.noreply.github.com>
2022-04-01 18:24:08 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,43 +2,49 @@ import nextcord #pip install nextcord
 from nextcord.ext import commands
 import os #pip install os
 
-bot  = commands.Bot(command_prefix = "!")
+bot  = commands.Bot(command_prefix = "your desired prefix")
 
 @bot.command(brief="Load a command")
+@commands.is_owner() #checks if the user owns the application
 async def load(ctx, extension):
-  bot.load_extension(f'cogs.{extension}')
+  bot.load_extension(f'cogs.{extension}') #loads cog
+
   embed = nextcord.Embed(
-    title = "Loaded 1 command",
-    description = "Successfully loaded 1 command!",
-    color = nextcord.Color.blurple()
+    description = f"Loaded ``{extension}`` successfully",
+    color = nextcord.Color.red()
   )
+
   await ctx.send(embed=embed)
 
 @bot.command(brief="Unload a command")
+@commands.is_owner()
 async def unload(ctx, extension):
   bot.unload_extension(f'cogs.{extension}')
+
   embed = nextcord.Embed(
-    title = "Unloaded 1 command",
-    description = "Successfully unloaded 1 command!",
-    color = nextcord.Color.blurple()
+    description = f"Unloaded ``{extension}`` successfully",
+    color = nextcord.Color.red()
   )
+
   await ctx.send(embed=embed)
 
 @bot.command(brief="Reload a command")
+@commands.is_owner()
 async def reload(ctx, extension):
   bot.unload_extension(f'cogs.{extension}')
   bot.load_extension(f'cogs.{extension}')
+
   embed = nextcord.Embed(
-    title = "Reloaded 1 command",
-    description = "Successfully reloaded 1 command!",
-    color = nextcord.Color.blurple()
+    description = f"Reloaded ``{extension}`` successfully",
+    color = nextcord.Color.red()
   )
+
   await ctx.send(embed=embed)
 
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     bot.load_extension(f'cogs.{filename[:-3]}')
-  
-bot.run('insert your bot token here')
+
+bot.run("replace this message with your bot token")
 
 # A simple Discord bot base made in Python by Toneh#3391 on Discord
